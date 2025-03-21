2a72b4d8ee866e2cec7687097919f9bb78f12966
parksehun <davidpark8575@gmail.com>
2025-03-21 23:29:02 +0900

    프로젝트 초기화

--- backend/nestjs/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GraphController } from './graph/graph.controller';
-import { GraphService } from './graph/graph.service';
-
-@Module({
-  controllers: [GraphController],
-  providers: [GraphService],
-})
-export class AppModule {}
