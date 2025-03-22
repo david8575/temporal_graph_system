628739d66fd9ab8d7fa0504dbacd76fab868d325
parksehun <davidpark8575@gmail.com>
2025-03-22 23:59:54 +0900

    .

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>THIS IS TEMPORAL GRAPH SYSTEM</h1>
+      <p>this is main page</p>
+    </div>
+  );
+}
+
+export default App;
